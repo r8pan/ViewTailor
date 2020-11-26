25bf035d95d1c9aa6124e9114daff1eab74fd9c1
br8558 <jssnjsprq@gmail.com>
2020-11-26 11:39:24 -0500

    Rewrote focusHighlight

--- src/initLoad.js
+++ src/initLoad.js
@@ -1,39 +1,15 @@
 import { update } from "./update.js";
 import { distinguishVisitedLinks } from "./contentScripts/distinguishVisitedLinks/distinguishVisitedLinks.js";
+import { focusHighlightCSS1, focusHighlightCSS2, focusHighlight } from "./contentScripts/focusHighlight/focusHighlight.js";
 
 export { initLoad };
 
-var focusHighlightCSS1 = "a:focus {color:rgb(255,255,255) !important; background-color:rgb(96,96,225) !important;" +
-    "outline-style:solid !important; outline-color:rgb(128,128,225) !important; outline-width:3px !important; padding:3px !important;" +
-    "border:none !important;}";
-var focusHighlightCSS2 = "a:focus div {color: rgb(255,255,255) !important;}";
 var url = (new URL(window.location.href)).hostname;
 
 function initLoad() {
     chrome.storage.local.get(url, data => {
         if (data[url] && data[url].focusHighlight) {
-            for (let i = document.styleSheets.length-1; i >= 0; i--) {
-                let styleSheet = document.styleSheets[i];
-                if (!styleSheet.href) {
-                    styleSheet.insertRule(focusHighlightCSS1, styleSheet.cssRules.length);
-                    styleSheet.insertRule(focusHighlightCSS2, styleSheet.cssRules.length);
-                    break;
-                }
-            }
-        }
-        else {
-            for (let i = document.styleSheets.length-1; i >= 0; i--) {
-                let styleSheet = document.styleSheets[i];
-                if (!styleSheet.href) {
-                    for (let j = styleSheet.cssRules.length-1; j >= 0; j--) {
-                        if(styleSheet.cssRules[j].selectorText && (
-                            styleSheet.cssRules[j].selectorText.startsWith("a:focus") ||
-                            styleSheet.cssRules[j].selectorText.startsWith("a:focus div"))) {
-                            styleSheet.removeRule(j);
-                        }
-                    }
-                }
-            }
+            focusHighlight(data[url], focusHighlightCSS1, focusHighlightCSS2);
         }
 
         if (data[url] && data[url].distinguishVisitedLinks) {
